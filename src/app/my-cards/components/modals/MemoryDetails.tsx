647086d4daf1aa12b335d599d3d5018a33a583d8
iueu <ilwelisaac@hotmail.com>
2023-09-10 22:23:50 -0300

    feat: styles ajustments

--- src/app/my-cards/components/modals/MemoryDetails.tsx
+++ src/app/my-cards/components/modals/MemoryDetails.tsx
@@ -12,7 +12,7 @@ export default function MemoryDetails(){
     open
     ?<div className="modal z-10">
       <div className="bg-surface h-2/3 w-2/3 rounded p-4 relative">
-        <XCircle onClick={() => dispatch(close())} color={"#eb6f92"} className="cursor-pointer absolute top-4 right-4 opacity-50 hover:opacity-100 duration-500"/>
+        <XCircle height={40} width={40} onClick={() => dispatch(close())} color={"#eb6f92"} className=" info"/>
         {id}
       </div>
     </div>
